2397c98d78729bdb6ded82c930c0f86a625f6a19
naakhurz <157807981+naakhurz@users.noreply.github.com>
2024-06-22 20:08:47 +0000

    Add files via upload

---
+++ src/pages/Player/index.js
@@ -0,0 +1,39 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import Banner from "components/Banner";
+import styles from "./Player.module.css"
+import Titulo from "components/Titulo";
+import { useParams } from "react-router-dom";
+import NotFound from "pages/NotFound";
+import { useEffect, useState } from "react";
+
+function Player(){
+ const [video,setVideo]= useState([])
+
+const parametros = useParams()
+useEffect(()=>{
+    fetch(`https://my-json-server.typicode.com/Naakhurz/alura-cinema-api/videos?id=${parametros.id}`)
+    .then(response=>response.json())
+    .then(data=>{
+        setVideo(...data)
+    })
+ },[])
+ 
+if(!video)return <NotFound/>
+    return(
+       <>
+       <Banner img="player" color="#58B9AE"/>
+        <Titulo>
+            <h1>Player</h1>
+        </Titulo>
+        <section className={styles.container}>
+        <iframe width="100%" height="100%" 
+        src={video.link} 
+        title={video.titulo} 
+        frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+        </section>
+       </>
+    )
+}
+
+export default Player;
